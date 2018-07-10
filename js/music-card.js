02313195fef1f561cb404b5eaadd67a1dc6f8402
Tyler <test@email.com>
2018-07-10 13:18:30 -0400

    Finished javascript for album preview section.

--- js/music-card.js
+++ js/music-card.js
@@ -1,39 +1,73 @@
-var songs = ["../media/youre_the_rock.wav", "../media/restoration.wav", "../media/march_on.wav"];
+var songs = ["../media/mourn.mp3", "../media/have_faith_in_god.mp3", "../media/will_you_trust_in_the_lord.mp3", "../media/glow.mp3", "../media/new_beginning.mp3", "../media/we_bow_down.mp3", "../media/march_on.mp3", "../media/youre_the_rock.mp3", "../media/restoration.mp3", "../media/they_dont.mp3"];
 
 var song = new Audio();
 var currentSong = 0;
+var i = 0;
 
 function playSong() {
   song.src = songs[currentSong];
   song.play();
 }
 
-function playTrackOne() {
-  currentSong = 0;
-  song.pause();
+function playTrack() {
   if (song.paused) {
+    currentSong = i;
+    playSong();
+  }
+  else if (currentSong != i) {
+    currentSong = i;
     playSong();
   }else{
     song.pause();
   }
+}
+
+function playTrackOne() {
+  i = 0;
+  playTrack();
 }
 
 function playTrackTwo() {
-  currentSong = 1;
-  song.pause();
-  if (song.paused) {
-    playSong();
-  }else{
-    song.pause();
-  }
+  i = 1;
+  playTrack();
 }
 
 function playTrackThree() {
-  currentSong = 2;
-  song.pause();
-  if (song.paused) {
-    playSong();
-  }else{
-    song.pause();
-  }
+  i = 2;
+  playTrack();
+}
+
+function playTrackFour() {
+  i = 3;
+  playTrack();
+}
+
+function playTrackFive() {
+  i = 4;
+  playTrack();
+}
+
+function playTrackSix() {
+  i = 5;
+  playTrack();
+}
+
+function playTrackSeven() {
+  i = 6;
+  playTrack();
+}
+
+function playTrackEight() {
+  i = 7;
+  playTrack();
+}
+
+function playTrackNine() {
+  i = 8;
+  playTrack();
+}
+
+function playTrackTen() {
+  i = 9;
+  playTrack();
 }
